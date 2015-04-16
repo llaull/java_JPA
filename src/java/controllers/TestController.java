2dbe59c5e97992e2290548c25fcb5250fa548fc6
llaull <laurent@llovem.eu>
2015-04-16 11:31:55 +0200

    Signed-off-by: llaull <laurent@llovem.eu>

--- src/java/controllers/TestController.java
+++ src/java/controllers/TestController.java
@@ -6,9 +6,12 @@ import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.EJB;
 import javax.enterprise.context.RequestScoped;
+import javax.faces.model.DataModel;
+import javax.faces.model.ListDataModel;
 import javax.inject.Named;
 
 /**
+ * pas de transactionnelle c le conteneur qui le gere
  *
  * @author MOi
  */
@@ -16,14 +19,27 @@ import javax.inject.Named;
 @RequestScoped
 public class TestController implements Serializable {
 
+    private DataModel items2 = null;
+
     @EJB
     private TestFacade ejTestFacade;
 
     public String prepareListFruit() {
         List<String> l = new ArrayList<>();
-        l = getEjTestFacade().findAll();
+        l = getEjTestFacade().lesFruits();
         System.out.println("-> " + l.size());
         return "index2";
+    }
+
+    private DataModel createPageDataModel2() {
+        return new ListDataModel(getEjTestFacade().findAll2());
+    }
+
+    public DataModel getItems2() {
+        if (items2 == null) {
+            items2 = createPageDataModel2();
+        }
+        return items2;
     }
 
     /**
--- src/java/facades/TestFacade.java
+++ src/java/facades/TestFacade.java
@@ -1,8 +1,12 @@
 package facades;
 
+import entities.Client;
 import java.util.ArrayList;
 import java.util.List;
 import javax.ejb.Stateless;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
 
 /**
  *
@@ -10,16 +14,27 @@ import javax.ejb.Stateless;
  */
 @Stateless
 public class TestFacade {
-    
-    
-    public List<String> findAll(){
+
+    @PersistenceContext
+    private EntityManager em;
+
+    public List<Client> findAll3() {
+        Query q = em.createNamedQuery("Client.findAll3");
+        return q.getResultList();
+    }
+
+    public List<Client> findAll2() {
+        Query q = em.createQuery("SELECT yo FROM Client yo");
+        return q.getResultList();
+    }
+
+    public List<String> lesFruits() {
         List<String> l = new ArrayList<>();
         l.add("Banane");
         l.add("Fruit du dragon");
         l.add("Noix de coco");
-        
+
         return l;
-        
     }
-    
+
 }
